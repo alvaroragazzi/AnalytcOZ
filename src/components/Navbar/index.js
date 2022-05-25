611c1cf0fbfe04b627176c29c70ba80dbb633730
alvaroragazzi <alvaroragazzi@gmail.com>
2022-05-25 19:17:46 -0300

    label com nome do usuario e botão de logout adicionados

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,12 +2,14 @@
 
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { SidebarData } from './SidebarData';
 import SubMenu from './SubMenu.js';
 import { IconContext } from 'react-icons/lib';
+import { IoMdPerson, IoIosLogOut } from "react-icons/io";
+import {logout} from "../../services/request";
+import { useNavigate } from "react-router-dom";
 
 const Nav = styled.div`
   background: #15171c;
@@ -49,6 +51,14 @@ const Sidebar = () => {
 
   const showSidebar = () => setSidebar(!sidebar);
 
+  const navigate = useNavigate();
+
+  const logoutReq = async() => {
+    logout();
+    localStorage.removeItem("info");
+    navigate("/login");
+  }
+
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
@@ -62,6 +72,13 @@ const Sidebar = () => {
             <NavIcon>
               <AiIcons.AiOutlineClose onClick={showSidebar} />
             </NavIcon>
+            <div style={{marginBottom: 10, marginLeft: 16, display: "flex", flexDirection: "row", alignContent: "center"}}>
+              <IoMdPerson style={{width: 19, height: 19, cursor: "pointer"}}/>
+              <h1 style={{color: "#fff", fontSize: 16, marginLeft: 5}}>Alvaro Ragazzi</h1>
+              <div style={{cursor: "pointer", marginLeft: 90}}>
+                <IoIosLogOut style={{width: 21, height: 21, marginBottom: 7}} onClick={logoutReq}/>
+              </div>
+            </div>
             {SidebarData.map((item, index) => {
               return <SubMenu item={item} key={index} />;
             })}
