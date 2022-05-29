1fa54169bc925fe470869e15fdaae6cb697de40d
alvaroragazzi <alvaroragazzi@gmail.com>
2022-05-29 20:19:20 -0300

    removido state de fechar navbar

--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -2,7 +2,9 @@ import React from "react";
 import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
 import * as RiIcons from "react-icons/ri";
+
 import { IoMdPersonAdd, IoIosPaper } from "react-icons/io";
+
 import { GoDiffAdded } from "react-icons/go";
 import { MdSell } from "react-icons/md";
 
