2d42cb04d0d1218d7a0bee33ab715be33801e02d
alvaroragazzi <alvaroragazzi@gmail.com>
2022-05-29 20:20:32 -0300

    configurações para o tema dos formulários

---
+++ src/components/FormTheme/index.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+export default createTheme({
+    status: {
+      danger: "#e53e3e",
+    },
+    palette: {
+      primary: {
+        main: "#15171c",
+        darker: "#000000",
+      },
+      neutral: {
+        main: "#64748B",
+        contrastText: "#fff",
+      },
+    },
+});
